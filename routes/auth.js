473c377ef1627c4546bc59fb4fc529fbca49ea74
Uncle798 <eric.branson@gmail.com>
2023-02-08 14:18:19 -0700

    added eslint

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var router = express.Router();
-const passport = require('passport')
+/* eslint-disable linebreak-style */
+const express = require('express');
+
+const router = express.Router();
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth');
-const MagicLoginStrategy = require('passport-magic-login')
+const MagicLoginStrategy = require('passport-magic-login');
 
-
-/*Google Login Redirect*/
-router.get('/login/federated/google', passport.authenticate('google'))
+/* Google Login Redirect */
+router.get('/login/federated/google', passport.authenticate('google'));
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('login', { title: 'Login to Moscow Ministorage' });
 });
 
