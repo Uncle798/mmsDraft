4433a203fd3fc1b6d39662857673f56f21a9836e
Uncle798 <eric.branson@gmail.com>
2023-02-15 14:21:44 -0700

    routing based on roles

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ensureLoggedIn } = require('connect-ensure-login');
 
 const router = express.Router();
 
@@ -17,25 +18,47 @@ router.get('/availableunits', (req, res, next) => {
 /* Admin Dashboard. */
 router.get(
   '/admin',
-  // ensureLogin({ redirectTo: '/auth/login' }),
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
   // eslint-disable-next-line no-unused-vars
-  (req, res, next) => res.render('adminDashboard', { title: 'Admin Dashboard' }),
+  (req, res, next) => {
+    if (req.user.isAdmin) {
+      res.render('adminDashboard', { title: 'Admin Dashboard' });
+    } else {
+      res.redirect('/customer');
+    }
+  },
 );
 
 /* Customer Dashboard. */
 router.get(
   '/customer',
-  // ensureLogin({ redirectTo: '/auth/login' }),
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
   // eslint-disable-next-line no-unused-vars
-  (req, res, next) => res.render('customerDashboard', { title: 'Customer Dashboard' }),
+  (req, res, next) => {
+    if (req.user.isAdmin) {
+      res.redirect('/admin');
+    } else if (req.user.isEmployee) {
+      res.redirect('/employee');
+    } else {
+      res.render('customerDashboard', { title: 'Customer Dashboard' });
+    }
+  },
 );
 
 /* Employee Dashboard. */
 router.get(
   '/employee',
-  // ensureLogin({ redirectTo: '/auth/login' }),
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
   // eslint-disable-next-line no-unused-vars
-  (req, res, next) => res.render('employeeDashboard', { title: 'Employee Dashboard' }),
+  (req, res, next) => {
+    if (req.user.isAdmin) {
+      res.redirect('/admin');
+    } else if (req.user.isEmployee) {
+      res.render('employeeDashboard', { title: 'Employee Dashboard' });
+    } else {
+      res.redirect('/customer')
+    }
+  },
 );
 
 module.exports = router;
